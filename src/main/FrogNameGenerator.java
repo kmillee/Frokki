037df62466dc71303290d5ebc2e3372068fdc229
kmillee <jouanet.camille@gmail.com>
2026-06-04 22:34:59 +0200

    pond frog spawning fixed

--- src/main/FrogNameGenerator.java
+++ src/main/FrogNameGenerator.java
@@ -2,6 +2,8 @@ package main;
 
 import java.io.File;
 import java.io.IOException;
+import java.io.InputStream;
+import java.nio.charset.StandardCharsets;
 import java.nio.file.Files;
 import java.util.ArrayList;
 import java.util.List;
@@ -21,23 +23,56 @@ public class FrogNameGenerator {
     }
     private FrogNameGenerator(){} // This class should not be instantiated
 
+//    private static List<String> getList(String fileName) {
+//        String path = Constants.RESOURCES_PATH + File.separator + "name" + File.separator + fileName;
+//
+//        try {
+//            String content = Files.readString(Path.of(path));
+//            String[] items = content.split("[,\\r\\n]+"); // Separate content by comma and new line
+//            List<String> list = new ArrayList<>();
+//            for (String item : items) {
+//                if (!item.isBlank()) list.add(item.trim());
+//            }
+//            return list;
+//        } catch(IOException e){
+//            System.err.println("Could not read file: " + e.getMessage());
+//        }
+//        return null;
+//    }
+
     private static List<String> getList(String fileName) {
-        String path = Constants.RESOURCES_PATH + File.separator + "name" + File.separator + fileName;
 
-        try {
-            String content = Files.readString(Path.of(path));
-            String[] items = content.split("[,\\r\\n]+"); // Separate content by comma and new line
-            List<String> list = new ArrayList<>();
-            for (String item : items) {
-                if (!item.isBlank()) list.add(item.trim());
+        String resourcePath =
+                "/name/" + fileName;
+
+        try (InputStream is =
+                     Utils.class.getResourceAsStream(resourcePath)) {
+
+            if (is == null) {
+                System.err.println("Resource not found: " + resourcePath);
+                return List.of();
             }
-            return list;
-        } catch(IOException e){
-            System.err.println("Could not read file: " + e.getMessage());
-        }
-        return null;
-    }
 
+            String content =
+                    new String(is.readAllBytes(), StandardCharsets.UTF_8);
+
+            String[] items = content.split("[,\\r\\n]+");
+
+            List<String> list = new ArrayList<>();
+
+            for (String item : items) {
+                if (!item.isBlank()) {
+                    list.add(item.trim());
+                }
+            }
+
+            return list;
+
+        } catch (IOException e) {
+            System.err.println("Could not read file: " + e.getMessage());
+            return List.of();
+        }
+    }
     public static String generateName(){
         String pattern = patterns.get((int)Math.floor(Math.random()*patterns.size()));
 
